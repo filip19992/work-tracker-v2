855101d5b9486aba8b0af17d2c9006d3c35f2954
filip19992 <filipwlodarczyk99@gmail.com>
2024-06-06 20:12:46 +0200

    Disabling cors

--- src/main/java/pl/filipwlodarczyk/worktrackerv2/security/SecurityConfig.java
+++ src/main/java/pl/filipwlodarczyk/worktrackerv2/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,6 +23,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(CorsConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/health", "/auth/register", "/auth/login", "/users/all").permitAll())
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())

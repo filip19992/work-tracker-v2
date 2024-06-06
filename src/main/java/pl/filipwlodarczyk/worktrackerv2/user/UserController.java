e4d7d7c4355281f3c32d3dff537b980ef42805c1
filip19992 <filipwlodarczyk99@gmail.com>
2024-06-06 16:29:47 +0200

    Adding ToDoService

--- src/main/java/pl/filipwlodarczyk/worktrackerv2/user/UserController.java
+++ src/main/java/pl/filipwlodarczyk/worktrackerv2/user/UserController.java
@@ -2,7 +2,9 @@ package pl.filipwlodarczyk.worktrackerv2.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +27,7 @@ public class UserController {
     }
 
     @GetMapping(value = "/username")
-    public String currentUserName(Principal principal) {
-        return principal.getName();
+    public String currentUserName(Authentication authentication) {
+        return authentication.getName();
     }
 }

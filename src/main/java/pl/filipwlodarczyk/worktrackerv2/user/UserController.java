f50ab98924f4ee48000d5b0740d8f1cbb53994a6
filip19992 <filipwlodarczyk99@gmail.com>
2024-06-06 14:59:50 +0200

    Added endpoint with usernames

--- src/main/java/pl/filipwlodarczyk/worktrackerv2/user/UserController.java
+++ src/main/java/pl/filipwlodarczyk/worktrackerv2/user/UserController.java
@@ -2,13 +2,24 @@ package pl.filipwlodarczyk.worktrackerv2.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-    private final PasswordEncoder encoder;
-    private final UserRepistory userRepistory;
+    private final UserServiceImpl userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<String>> getUsers() {
+        var result = userService.getUsers();
+
+        return ResponseEntity.ok(result);
+    }
 }
--- src/main/java/pl/filipwlodarczyk/worktrackerv2/user/UserServiceImpl.java
+++ src/main/java/pl/filipwlodarczyk/worktrackerv2/user/UserServiceImpl.java
@@ -7,6 +7,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserDetailsService {
@@ -15,5 +19,15 @@ public class UserServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepistory.findByUsername(username).orElseThrow();
+    }
+
+    public List<String> getUsers() {
+        var usernames = new ArrayList<String>();
+        var allUsers = userRepistory.findAll();
+
+        allUsers.forEach(c -> usernames.add(c.getUsername()));
+
+        return usernames;
+
     }
 }
